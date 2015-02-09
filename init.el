c98c10ecaebb0f2b04a78739a636705ab43d40ea
Pedro Semeano <pedro.semeano@tripwell.com>
2015-02-09 09:40:33 +0100

    Improved configuration

--- init.el
+++ init.el
@@ -46,7 +46,7 @@
  '(cursor-color "#839496")
  '(custom-safe-themes
    (quote
-    ("64581032564feda2b5f2cf389018b4b9906d98293d84d84142d90d7986032d33" "72407995e2f9932fda3347e44e8c3f29879c5ed88da71f06ba4887b0596959a4" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4217c670c803e8a831797ccf51c7e6f3a9e102cb9345e3662cc449f4c194ed7d" "49eea2857afb24808915643b1b5bd093eefb35424c758f502e98a03d0d3df4b1" "b7d8113de2f7d9a3cf42335d8eed8415b5a417e7f6382e59076f9f4ae4fa4cee" "9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "57f8801351e8b7677923c9fe547f7e19f38c99b80d68c34da6fa9b94dc6d3297" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e194b1010c026b1401146e24a85e4b7c545276845fc38b8c4b371c8338172ad" "8f7e1668dd3a097964e6016c26d36822ab2e48fc3e9a3a2e2634224a5ca728c8" "bd115791a5ac6058164193164fd1245ac9dc97207783eae036f0bfc9ad9670e0" "8fd393097ac6eabfcb172f656d781866beec05f27920a0691e8772aa2cdc7132" default)))
+    ("4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" "64581032564feda2b5f2cf389018b4b9906d98293d84d84142d90d7986032d33" "72407995e2f9932fda3347e44e8c3f29879c5ed88da71f06ba4887b0596959a4" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4217c670c803e8a831797ccf51c7e6f3a9e102cb9345e3662cc449f4c194ed7d" "49eea2857afb24808915643b1b5bd093eefb35424c758f502e98a03d0d3df4b1" "b7d8113de2f7d9a3cf42335d8eed8415b5a417e7f6382e59076f9f4ae4fa4cee" "9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "57f8801351e8b7677923c9fe547f7e19f38c99b80d68c34da6fa9b94dc6d3297" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e194b1010c026b1401146e24a85e4b7c545276845fc38b8c4b371c8338172ad" "8f7e1668dd3a097964e6016c26d36822ab2e48fc3e9a3a2e2634224a5ca728c8" "bd115791a5ac6058164193164fd1245ac9dc97207783eae036f0bfc9ad9670e0" "8fd393097ac6eabfcb172f656d781866beec05f27920a0691e8772aa2cdc7132" default)))
  '(diredp-hide-details-initially-flag nil)
  '(fci-rule-color "#49483E")
  '(foreground-color "#839496")
@@ -63,6 +63,7 @@
      ("#A41F99" . 85)
      ("#49483E" . 100))))
  '(magit-diff-options (quote ("--ignore-space-change" "--minimal")))
+ '(magit-diff-use-overlays nil)
  '(org-agenda-files (quote ("~/Projects/pets.org")))
  '(org-hide-leading-stars nil)
  '(org-pretty-entities t)
@@ -181,11 +182,14 @@
 ;; delete tab, not converting to space
 (defvar backward-delete-char-untabify 'nil)
 
-(require 'dired-x)
-(setq-default dired-omit-files-p t) ; this is buffer-local variable
-(setq dired-omit-files
-      (concat dired-omit-files "\\|^\\..+$"))
-(setq dired-use-ls-dired t)
+
+(use-package dired-x
+  :config
+  (setq-default dired-omit-files-p t) ; this is buffer-local variable
+  (setq dired-omit-files
+	(concat dired-omit-files "\\|^\\..+$"))
+  (setq dired-use-ls-dired t)
+  )
 
 ;; tab modes
 ;; to setup tabs
@@ -229,6 +233,11 @@
              (setq indent-tabs-mode nil)
              )
           )
+
+(add-hook 'go-mode-hook
+	  (lambda ()
+	    (setq tab-width 2)
+	    ))
 
 ;; Use "y or n" for answers instead of complete words "yes or no"
 (fset 'yes-or-no-p 'y-or-n-p)
@@ -316,8 +325,30 @@ This functions should be added to the hooks of major modes for programming."
 ;; hide others with cmd-alt-h
 (global-set-key (kbd "M-A-h") 'ns-do-hide-others)
 
+(defun endless/comment-line (n)
+  "Comment or uncomment current line and leave point after it.
+With positive prefix, apply to N lines including current one.
+With negative prefix, apply to -N lines above."
+  (interactive "p")
+  (comment-or-uncomment-region
+   (line-beginning-position)
+   (goto-char (line-end-position n)))
+  (forward-line 1)
+  (back-to-indentation))
+
+(global-set-key (kbd "C-;") #'endless/comment-line)
+
+(setenv "GOPATH" "/Users/pedro/Projects/go/space")
+
+(use-package paradox
+  :init
+  (setq paradox-github-token "b9b60d27e4b2e92bf5aa54d11c4ce03a5681d6f7")
+  )
+
+
 ;; call dash CTRL-C d
-(global-set-key (kbd "C-c d") 'dash-at-point)
+(use-package dash
+  :bind ("C-c d" . dash-at-point))
 
 ;; change exec path to use /usr/local/bin before everything
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
@@ -329,8 +360,14 @@ This functions should be added to the hooks of major modes for programming."
 )
 
 ;; display the function definition
-(which-function-mode)
-(defvar which-func-unknown "n/a") ;; display n/a instead of XXX
+;; (which-function-mode)
+;; (defvar which-func-unknown "n/a") ;; display n/a instead of XXX
+;; (setq-default header-line-format
+;;               '((which-func-mode ("" which-func-format " "))))
+;; (setq mode-line-misc-info
+;;       ;; We remove Which Function Mode from the mode line, because it's mostly
+;;       ;; invisible here anyway.
+;;       (assq-delete-all 'which-func-mode mode-line-misc-info))
 
 (setq system-uses-terminfo nil)
 
@@ -347,7 +384,7 @@ This functions should be added to the hooks of major modes for programming."
 (global-set-key (kbd "C-a") 'smart-beginning-of-line)
 
 (global-hl-line-mode 1)
-(set-face-background 'hl-line "#2D2D2D")
+(set-face-background 'hl-line "#1D1D1D")
 
 ;; turn off toolbar
 (tool-bar-mode -1)
@@ -373,7 +410,8 @@ This functions should be added to the hooks of major modes for programming."
 (use-package monokai-theme
   :ensure t
   :config (load-theme 'monokai t)
-  :init (set-frame-font "-*-DejaVu Sans Mono-light-normal-normal-*-10-*-*-*-m-0-iso10646-1"))
+  ;; :init (set-frame-font "-*-DejaVu Sans Mono-light-normal-normal-*-10-*-*-*-m-0-iso10646-1"))
+  )
 
 (use-package desktop
   :config
@@ -474,10 +512,9 @@ This functions should be added to the hooks of major modes for programming."
   :bind ("C-." . company-complete)
   :config
   (global-company-mode 1)
-  (progn
-    (define-key company-mode-map (kbd "C-.") 'helm-company)
-    (define-key company-active-map (kbd "C-.") 'helm-company))
 )
+
+;; (global-key-binding (kbd "C-.") 'company-complete)
 
 (use-package visual-regexp
   :ensure t
@@ -583,6 +620,9 @@ This functions should be added to the hooks of major modes for programming."
   (key-chord-define-global "jj" 'ace-jump-mode)
   (key-chord-define-global "jl" 'ace-jump-line-mode)
   (key-chord-mode +1))
+
+(use-package rust-mode
+  :ensure t)
 
 ;; After packages init configuration
 
